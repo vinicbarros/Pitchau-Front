99ef3298cad5d489ac067f99086d12d65d11c9d9
luciano <lpereira2306@gmail.com>
2022-09-19 17:14:31 -0300

    logo finalizado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,141 +1,148 @@
-import styled from "styled-components";
-import { FaShoppingCart } from "react-icons/fa";
-import { AiOutlineSearch } from "react-icons/ai";
-import { ImExit } from "react-icons/im";
-import { useContext, useEffect } from "react";
-import { getCartUser } from "../services/Pitchau";
-import UserContext from "../contexts/UserContext";
-import { Link, useNavigate } from "react-router-dom";
+import styled from 'styled-components';
+import { FaShoppingCart } from 'react-icons/fa';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { ImExit } from 'react-icons/im';
+import { useContext, useEffect } from 'react';
+import { getCartUser } from '../services/Pitchau';
+import UserContext from '../contexts/UserContext';
+import { Link, useNavigate } from 'react-router-dom';
+import logoPitchau from '../assets/images/pitchauLogo.jpeg';
 
 export default function Navbar() {
-  const { setUserCartList, userCartList, clicked } = useContext(UserContext);
-  const navigate = useNavigate();
+	const { setUserCartList, userCartList, clicked } = useContext(UserContext);
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const userCart = await getCartUser();
-        setUserCartList(userCart.data.userCartList);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    fetchData();
-  }, [clicked]);
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const userCart = await getCartUser();
+				setUserCartList(userCart.data.userCartList);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		fetchData();
+	}, [clicked]);
 
-  function exitUser() {
-    localStorage.removeItem("pitchau");
-    navigate("/sign-in");
-  }
+	function exitUser() {
+		localStorage.removeItem('pitchau');
+		navigate('/sign-in');
+	}
 
-  return (
-    <ContentHeader>
-      <Link to="/">
-        <Title>πtchau</Title>
-      </Link>
-      <InputBox>
-        <SearchBar placeholder="Busque aqui"></SearchBar>
-        <div>
-          <AiOutlineSearch />
-        </div>
-      </InputBox>
-      <Wrapper>
-        <CartBox
-          onClick={() => {
-            console.log("to aqui");
-            navigate("/cart");
-          }}
-        >
-          <FaShoppingCart size="1.5em" color="rgb(107,158,208)" />
-          {userCartList.length === 0 ? <></> : <div>{userCartList.length}</div>}
-        </CartBox>
-        <ImExit
-          onClick={exitUser}
-          style={{
-            marginLeft: "30px",
-            cursor: "pointer",
-          }}
-          size="1.5em"
-          color="rgb(107,158,208)"
-        />
-      </Wrapper>
-    </ContentHeader>
-  );
+	return (
+		<ContentHeader>
+			<Link to='/'>
+				<Title>
+					<img src={logoPitchau} alt='' />
+				</Title>
+			</Link>
+			<InputBox>
+				<SearchBar placeholder='Busque aqui'></SearchBar>
+				<div>
+					<AiOutlineSearch />
+				</div>
+			</InputBox>
+			<Wrapper>
+				<CartBox
+					onClick={() => {
+						console.log('to aqui');
+						navigate('/cart');
+					}}
+				>
+					<FaShoppingCart size='1.5em' color='rgb(107,158,208)' />
+					{userCartList.length === 0 ? <></> : <div>{userCartList.length}</div>}
+				</CartBox>
+				<ImExit
+					onClick={exitUser}
+					style={{
+						marginLeft: '30px',
+						cursor: 'pointer',
+					}}
+					size='1.5em'
+					color='rgb(107,158,208)'
+				/>
+			</Wrapper>
+		</ContentHeader>
+	);
 }
 
 const ContentHeader = styled.header`
-  position: fixed;
-  z-index: 100;
-  top: 0;
-  left: 0;
-  font-family: "Poppins";
-  width: 100vw;
-  height: 6em;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  background-color: rgb(0, 96, 177);
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+	position: fixed;
+	z-index: 100;
+	top: 0;
+	left: 0;
+	font-family: 'Poppins';
+	width: 100vw;
+	height: 6em;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	background-color: rgb(0, 96, 177);
+	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
 
-  a {
-    text-decoration: none;
-  }
+	a {
+		text-decoration: none;
+	}
 `;
 
 const Title = styled.h1`
-  font-size: 36px;
-  font-weight: 500;
-  color: #ffffff;
-  margin-left: 32px;
+	font-size: 36px;
+	font-weight: 500;
+	color: #ffffff;
+	margin-left: 32px;
+
+	img {
+		width: 300px;
+	}
 `;
 
 const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  margin-right: 32px;
+	display: flex;
+	align-items: center;
+	margin-right: 32px;
 `;
 
 const CartBox = styled.div`
-  display: flex;
-  position: relative;
+	display: flex;
+	position: relative;
 
-  div {
-    right: -10px;
-    top: -5px;
-    position: absolute;
-    font-weight: bold;
-    font-size: 12px;
-    width: 16px;
-    height: 16px;
-    background-color: #e35214;
-    border-radius: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: #ffffff;
-  }
+	div {
+		right: -10px;
+		top: -5px;
+		position: absolute;
+		font-weight: bold;
+		font-size: 12px;
+		width: 16px;
+		height: 16px;
+		background-color: #e35214;
+		border-radius: 50%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		color: #ffffff;
+	}
 `;
 
 const InputBox = styled.form`
-  position: relative;
-  width: 40%;
-  display: flex;
-  align-items: center;
-  div {
-    position: absolute;
-    right: 1em;
-  }
+	position: relative;
+	width: 40%;
+	display: flex;
+	align-items: center;
+	div {
+		position: absolute;
+		right: 1em;
+	}
 `;
 
 const SearchBar = styled.input`
-  border: none;
-  font-size: 0.875rem;
-  line-height: 1.3125rem;
-  position: relative;
-  width: 100%;
-  height: 1.25rem;
-  padding: 0.75rem 1rem;
-  background: rgb(255, 255, 255);
-  color: rgb(66, 70, 77);
-  border-radius: 0.25rem;
+	border: none;
+	font-size: 0.875rem;
+	line-height: 1.3125rem;
+	position: relative;
+	width: 100%;
+	height: 1.25rem;
+	padding: 0.75rem 1rem;
+	background: rgb(255, 255, 255);
+	color: rgb(66, 70, 77);
+	border-radius: 0.25rem;
 `;
